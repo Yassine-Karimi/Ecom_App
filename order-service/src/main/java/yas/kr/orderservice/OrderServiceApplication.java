a6eaa9d34daacc565a7fc790b64a4ae53329a7d6
Yassine Karimi <yassinkarimi.1.23@gmail.com>
2023-10-23 22:05:41 +0100

    2nd commit

--- order-service/src/main/java/yas/kr/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/yas/kr/orderservice/OrderServiceApplication.java
@@ -2,7 +2,10 @@ package yas.kr.orderservice;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import yas.kr.orderservice.entities.Order;
@@ -23,6 +26,7 @@ import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableFeignClients
+
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
